329532765cee9857a6a43b9638e2abe85ef9d27b
dabai-cyn <599283258@qq.com>
2019-06-29 00:21:34 +0800

    first commit

---
+++ src/main/java/com/cyn/service/HelloProperties.java
@@ -0,0 +1,17 @@
+package com.cyn.service;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "hello")
+public class HelloProperties {
+
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
---
+++ src/main/java/com/cyn/service/HelloService.java
@@ -0,0 +1,18 @@
+package com.cyn.service;
+
+public class HelloService {
+
+    HelloProperties helloProperties;
+
+    public HelloProperties getHelloProperties() {
+        return helloProperties;
+    }
+
+    public void setHelloProperties(HelloProperties helloProperties) {
+        this.helloProperties = helloProperties;
+    }
+
+    public String sayHello(String name ) {
+        return helloProperties.getMsg()+ "-" + name;
+    }
+}
